8c569f95023097cfaf53e642a481fbda748eb7bb
TDbouk <toufic.dbouk@gmail.com>
2017-04-19 10:46:28 +0300

    Initial commit

---
+++ app/src/main/java/com/test/myapplication/MainActivity.java
@@ -0,0 +1,58 @@
+package com.test.myapplication;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements BlankFragment.OnFragmentInteractionListener {
+
+
+    private FragmentManager mFragmentManager;
+    private Fragment mFragment;
+    private FragmentTransaction mTransaction;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mFragment = new BlankFragment();
+                    mTransaction = mFragmentManager.beginTransaction();
+                    mTransaction.replace(R.id.content, mFragment).commit();
+                    return true;
+                case R.id.navigation_dashboard:
+
+                    return true;
+                case R.id.navigation_notifications:
+
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFragmentManager = getSupportFragmentManager();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(0);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
